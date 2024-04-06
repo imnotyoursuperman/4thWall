e78d69f0acbace4de34bde7e9d94df45ff3cdf83
Kent Clark <123401423+imnotyoursuperman@users.noreply.github.com>
2024-04-06 21:00:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+DALLE_API_KEY = os.getenv('DALLE_API_KEY')

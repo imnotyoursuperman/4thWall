363ba4a66a67276ee316eea8e2314c7e83eb861b
Kent Clark <123401423+imnotyoursuperman@users.noreply.github.com>
2024-04-07 16:36:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ import uuid
 # Initialize your Pyrogram client with your credentials
 app = Client(
     "my_bot",
-    api_id=API_ID,
-    api_hash="API_HASH",
-    bot_token="BOT_TOKEN"
+    api_id=21927988,
+    api_hash="e18f720acdff1e5b0ec80616aecd8a5a",
+    bot_token="6878710104:AAGxeHBjlk8UjZ_cPbaW_74aySweKLDZl4w"
 )
 
 # Owner's user ID (Replace with your Telegram user ID)
